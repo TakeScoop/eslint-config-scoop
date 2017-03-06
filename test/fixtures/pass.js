2f80aabaa2ef121db7145b9f56bfb08f6f446e21
Scott Hulbert <scott@scotthulbert.com>
2017-03-05 22:49:13 -0800

    test

---
+++ test/fixtures/pass.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const fs = require('fs')
+
+const cased = require('./complexCaseFile')
+
+function main() {
+    const s = fs.constants
+    process.stderr.write(s)
+    process.stderr.write(cased)
+}
+
+main()
