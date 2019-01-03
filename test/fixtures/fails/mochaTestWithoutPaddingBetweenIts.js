26a8e0cd7f7659cc0aea2381fe30234c4c6019ba
Scott Hulbert <scott@scotthulbert.com>
2019-01-03 14:24:12 -0800

    mocha examples

---
+++ test/fixtures/fails/mochaTestWithoutPaddingBetweenIts.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const describe = function() {}
+const it = function() {}
+
+describe('really cool test suite', function() {
+    it('should pass', function(done) {
+        done()
+    })    
+    it('should pass again and notice there is padding above', function(done) {
+        done()
+    })
+})
