380d1c91ab443f23d5c21ae78704b5ed107ab741
Ben Drucker <bvdrucker@gmail.com>
2021-11-15 11:20:10 -0800

    update test runner for eslint@8

--- test/tests/index.js
+++ test/tests/index.js
@@ -1,18 +1,18 @@
 'use strict'
 
-const fs = require('fs')
+const { promises: fs, readdirSync } = require('fs')
 const path = require('path')
 
 const Code = require('code')
-const CLIEngine = require('eslint').CLIEngine
+const { ESLint } = require('eslint')
 
-const engine = new CLIEngine({
-    configFile: path.join(__dirname, '../../index.js')
+const eslint = new ESLint({
+    overrideConfigFile: path.join(__dirname, '../../index.js')
 })
 
 const fixturesDirectory = path.join(__dirname, '../fixtures')
 
-describe('testing eslint configuration', function() {
+describe('eslint configuration', function() {
     
     describe('failure cases', function() {
         
@@ -77,64 +77,54 @@ describe('testing eslint configuration', function() {
             },
         ]
         
-        it('should have a codified failure case for every fail fixture', function(done) {
-            fs.readdir(path.join(fixturesDirectory, '/fails'), function(err, filenames) {
-                if (err) {
-                    throw err
-                }
-            
-                const failureCaseFilenames = failureCases.map(failureCase => failureCase.filename)
-                expect(failureCaseFilenames.length).to.be.at.least(1)
-            
-                filenames.forEach(function(filename) {
-                    expect(failureCaseFilenames).to.include(filename)
-                })
-            
-                done()
+        it('should have a codified failure case for every fail fixture', async function() {
+            const filenames = await fs.readdir(path.join(fixturesDirectory, '/fails'))
+
+            const failureCaseFilenames = failureCases.map(failureCase => failureCase.filename)
+            expect(failureCaseFilenames.length).to.be.at.least(1)
+
+            filenames.forEach(function(filename) {
+                expect(failureCaseFilenames).to.include(filename)
             })
         })
 
         failureCases.forEach(function(test) {
-            it(`should fail ${test.filename} for violating ${castArray(test.rulename).join(' or ')}`, function(done) {
+            it(`should fail ${test.filename} for violating ${castArray(test.rulename).join(' or ')}`, async function() {
                 const filepath = path.join(fixturesDirectory, '/fails', test.filename)
-                const report = engine.executeOnFiles([filepath])
+                const [report] = await eslint.lintFiles([filepath])
             
                 expect(report.errorCount, 'ESLint error count').to.be.at.least(1)
             
                 const linesWithFailures = castArray(test.line)
                 const rulesThisCanViolate = castArray(test.rulename)
                 
-                expect(report.results).to.be.length(1)
-                expect(report.results[0].messages).to.be.length(linesWithFailures.length)
+                expect(report.messages).to.be.length(linesWithFailures.length)
             
-                report.results[0].messages.forEach(function(message) {
+                report.messages.forEach(function(message) {
                     expect(rulesThisCanViolate).to.include(message.ruleId)
                     expect(linesWithFailures, 'Defined line numbers should include error line').to.include(message.line)
                 })
-            
-                done()
             })
         })
     })
     
     describe('passing cases', function() {
-        const filenames = fs.readdirSync(path.join(fixturesDirectory, '/passes'))
+        const filenames = readdirSync(path.join(fixturesDirectory, '/passes'))
+
         filenames.forEach(function(filename) {
             const caseName = filename.replace(/\.js$/, '')
             
-            it(`test case '${caseName}' should pass`, function(done) {
+            it(`test case '${caseName}' should pass`, async function() {
                 const filepath = path.join(fixturesDirectory, '/passes', filename)
-                const report = engine.executeOnFiles([filepath])
+                const [report] = await eslint.lintFiles([filepath])
 
-                const errorStrings = report.results[0].messages.map(function(message) {
+                const errorStrings = report.messages.map(function(message) {
                     return `\tAt line ${message.line} column ${message.column}: ${message.message} (${message.ruleId})`
                 })
                 
                 if (report.errorCount > 0) {
                     Code.fail(`${report.errorCount} linting errors found:\n${errorStrings.join('\n')}`)
                 }
-                
-                done()
             })
         })
     })
